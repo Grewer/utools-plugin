71ed31b3679fc47931a04ac3793b4b0f8c5c688a
grewer <grewer@grewer.cn>
2021-09-07 00:28:43 +0800

    feat: 按钮操作

--- packages/jsMock/src/App.tsx
+++ packages/jsMock/src/App.tsx
@@ -2,17 +2,20 @@ import { Component, createSignal, onMount } from 'solid-js'
 import { debounce } from './utils/debounce'
 import Mock from 'mockjs'
 
+// @ts-ignore
 window.Mock = Mock
 
-const App: Component = () => {
-  console.log('render App')
-  const [result, setResult] = createSignal('')
-  const [input, setInput] = createSignal(`{
+
+const template = `{
   "list|5-10": [{
     name: '@cname'
   }],
 }`
-  )
+
+const App: Component = () => {
+  console.log('render App')
+  const [result, setResult] = createSignal('')
+  const [input, setInput] = createSignal(template)
 
   const onChange = debounce((val) => {
     try {
@@ -32,11 +35,7 @@ const App: Component = () => {
   }
 
   onMount(() => {
-    onChange(`{
-      "list|5-10": [{
-        name: '@cname'
-      }],
-    }`)
+    onChange(template)
   })
 
   const formatHandle = () => {
@@ -48,16 +47,15 @@ const App: Component = () => {
   }
 
   const resetHandle = () => {
-    setInput(`{
-  "list|5-10": [{
-    name: '@cname'
-  }],
-}`)
-    onChange(`{
-      "list|5-10": [{
-        name: '@cname'
-      }],
-    }`)
+    setInput(template)
+    onChange(template)
+  }
+
+  const copy = () => {
+    const res = window.utools?.copyText(result())
+    if (res) {
+      window.utools?.showNotification('已复制')
+    }
   }
 
   return (
@@ -65,11 +63,13 @@ const App: Component = () => {
       <div className="row">
         <button onClick={formatHandle} class="button button-clear">格式化</button>
         <button onClick={resetHandle} class="button button-clear">重置</button>
+        <button onClick={copy} class="button button-clear">复制结果</button>
       </div>
       <div class="row">
         <div class="column column-50">
-    <textarea value={input()} onInput={onInput} spellcheck autofocus placeholder={`输入 mock, 如{'boolean|1-2': true}`}
-              id="source"/>
+          <textarea value={input()} onInput={onInput} spellcheck autofocus
+                    placeholder={`输入 mock, 如{'boolean|1-2': true}`}
+                    id="source"/>
         </div>
         <div class="column column-50">
           <textarea readOnly value={result()}/>
